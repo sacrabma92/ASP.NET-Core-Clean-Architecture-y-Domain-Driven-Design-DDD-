3e8afaf6e4d7575ca1826569936b41642db075e4
Carlos Ramirez <karlossander92@gmail.com>
2023-07-09 18:53:24 -0500

    Patron Mediator

---
+++ CleanArchitecture/CleanArchitecture.Application/Contracts/Persistence/IAsyncRepository.cs
@@ -0,0 +1,26 @@
+﻿using CleanArchitecture.Domain.Common;
+using System.Linq.Expressions;
+
+namespace CleanArchitecture.Application.Contracts.Persistence
+{
+    public interface IAsyncRepository<T> where T : BaseDomainModel
+    {
+        // Metodos genericos para obtener la data de la BD
+        Task<IReadOnlyList<T>> GetAllAsync();
+        Task<IReadOnlyList<T>> GetAsync(Expression<Func<T, bool>> predicate);
+        Task<IReadOnlyList<T>> GetAsync(Expression<Func<T, bool>> predicate = null,
+                        Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
+                        string includeString = null,
+                        bool disableTracking = true);
+        Task<IReadOnlyList<T>> GetAsync(Expression<Func<T, bool>> predicate = null,
+                Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
+                List<Expression<Func<T,object>>> includes = null,
+                bool disableTracking = true);
+        Task<T> GetByIdAsync(int id);
+
+        // Metodos para manipular la Data. Actualizar, Agregar o eliminar
+        Task<T> AddAsync(T entity);
+        Task<T> UpdateAsync(T entity);
+        Task DeleteAsync(T entity);
+    }
+}
--- CleanArchitecture/CleanArchitecture.Domain/VideosActores.cs
+++ CleanArchitecture/CleanArchitecture.Domain/VideosActores.cs
@@ -1,6 +1,8 @@
-﻿namespace CleanArchitecture.Domain
+﻿using CleanArchitecture.Domain.Common;
+
+namespace CleanArchitecture.Domain
 {
-    public class VideosActores
+    public class VideosActores : BaseDomainModel
     {
         public int VideoId { get; set; }
         public int ActorId { get; set; }
